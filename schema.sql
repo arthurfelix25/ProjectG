4bcf147fa265ed78f2458dc4fa741ebdaabd087f
Arthur <arthur.felixa25@gmail.com>
2026-08-16 12:54:35 -0300

    feat: criando projeto

--- schema.sql
+++ schema.sql
@@ -1,17 +1,17 @@
-DROP TABLE IF EXISTS value;
+DROP TABLE IF EXISTS info;
 DROP TABLE IF EXISTS game;
 DROP TABLE IF EXISTS publisher;
 
 CREATE TABLE publisher
 (
     publisher_id INT PRIMARY KEY NOT NULL,
-    name VARCHAR(100) NOT NULL
+    publisher_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE game
 (
     game_id INT PRIMARY KEY NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     year INT,
     platform VARCHAR(50),
     genre VARCHAR(50),
@@ -19,13 +19,13 @@ CREATE TABLE game
     FOREIGN KEY (publisher_id) REFERENCES publisher(publisher_id)
 );
 
-create table value
+create table info
 (
-    val_id INT PRIMARY KEY NOT NULL,
+    info_id INT PRIMARY KEY NOT NULL,
     game_id INT NOT NULL,
-    globals DECIMAL(10, 2),
-    critics DECIMAL(5, 2),
-    users DECIMAL(5, 2),
+    global_sale_millions DECIMAL(10, 2),
+    critic_score DECIMAL(5, 2),
+    user_score DECIMAL(5, 2),
     rating VARCHAR(5),
     FOREIGN KEY (game_id) REFERENCES game(game_id)
 );
